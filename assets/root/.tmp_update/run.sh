95e6396ea0e6f71ae5a223c1707e7b0b1b1f8fab
Wei Wen Goh <goweiwen@gmail.com>
2023-06-12 17:51:00 +0800

    chore: remove run.sh, freeing up 1 process

--- assets/root/.tmp_update/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-DIR="$(dirname "$0")"
-cd "$DIR"
-
-# run Allium
-while true; do
-	RUST_BACKTRACE=1 RUST_LOG=trace /mnt/SDCARD/.allium/alliumd > /mnt/SDCARD/.allium/alliumd.log 2>&1
-done
