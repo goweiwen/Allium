7a4be3abcdb613c846901d7c26d37c6fdde8736e
Goh Wei Wen <goweiwen@gmail.com>
2023-06-30 05:39:56 +0800

    feat: add 750x640 test bg

--- common/src/platform/simulator.rs
+++ common/src/platform/simulator.rs
@@ -70,7 +70,7 @@ impl Platform for SimulatorPlatform {
 
     fn display(&mut self) -> Result<SimulatorWindow> {
         let display =
-            SimulatorDisplay::load_png("assets/simulator/ingame-mm.png").unwrap_or_else(|_| {
+            SimulatorDisplay::load_png("assets/simulator/bg-640x480.png").unwrap_or_else(|_| {
                 SimulatorDisplay::with_default_color(
                     Size::new(SCREEN_WIDTH, SCREEN_HEIGHT),
                     Color::new(0, 0, 0),
