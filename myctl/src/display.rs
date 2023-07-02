527324ac48a440ce6a807f866da6353766e5d500
Goh Wei Wen <goweiwen@gmail.com>
2023-07-02 20:48:50 +0800

    feat(myctl): add display stub

---
+++ myctl/src/display.rs
@@ -0,0 +1,52 @@
+#![allow(unreachable_code, unused_variables)]
+use anyhow::Result;
+
+pub fn set_brightness(brightness: i32) -> Result<()> {
+    todo!();
+    Ok(())
+}
+
+pub fn get_brightness() -> Result<i32> {
+    todo!();
+    Ok(0)
+}
+
+pub fn set_lumination(lumination: i32) -> Result<()> {
+    todo!();
+    Ok(())
+}
+
+pub fn get_lumination() -> Result<i32> {
+    todo!();
+    Ok(0)
+}
+
+pub fn set_hue(hue: i32) -> Result<()> {
+    todo!();
+    Ok(())
+}
+
+pub fn get_hue() -> Result<i32> {
+    todo!();
+    Ok(0)
+}
+
+pub fn set_saturation(saturation: i32) -> Result<()> {
+    todo!();
+    Ok(())
+}
+
+pub fn get_saturation() -> Result<i32> {
+    todo!();
+    Ok(0)
+}
+
+pub fn set_contrast(contrast: i32) -> Result<()> {
+    todo!();
+    Ok(())
+}
+
+pub fn get_contrast() -> Result<i32> {
+    todo!();
+    Ok(0)
+}
--- myctl/src/main.rs
+++ myctl/src/main.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 use clap::{arg, value_parser, Command};
 use simple_logger::SimpleLogger;
 
+mod display;
 mod volume;
 
 fn cli() -> Command {
@@ -11,8 +12,50 @@ fn cli() -> Command {
         .arg_required_else_help(true)
         .allow_external_subcommands(true)
         .subcommand(
-            Command::new("volume")
-                .arg(arg!([VOLUME] "Volume to set").value_parser(value_parser!(i32))),
+            Command::new("volume").arg(
+                arg!([VOLUME] "Volume to set")
+                    .allow_negative_numbers(true)
+                    .value_parser(value_parser!(i32)),
+            ),
+        )
+        .subcommand(
+            Command::new("display")
+                .arg_required_else_help(true)
+                .subcommand(
+                    Command::new("brightness").arg(
+                        arg!([BRIGHTNESS] "Brightness to set")
+                            .allow_negative_numbers(true)
+                            .value_parser(value_parser!(i32)),
+                    ),
+                )
+                .subcommand(
+                    Command::new("lumination").arg(
+                        arg!([LUMINATION] "Lumination to set")
+                            .allow_negative_numbers(true)
+                            .value_parser(value_parser!(i32)),
+                    ),
+                )
+                .subcommand(
+                    Command::new("hue").arg(
+                        arg!([HUE] "Hue to set")
+                            .allow_negative_numbers(true)
+                            .value_parser(value_parser!(i32)),
+                    ),
+                )
+                .subcommand(
+                    Command::new("saturation").arg(
+                        arg!([SATURATION] "Saturation to set")
+                            .allow_negative_numbers(true)
+                            .value_parser(value_parser!(i32)),
+                    ),
+                )
+                .subcommand(
+                    Command::new("contrast").arg(
+                        arg!([CONTRAST] "Contrast to set")
+                            .allow_negative_numbers(true)
+                            .value_parser(value_parser!(i32)),
+                    ),
+                ),
         )
 }
 
@@ -27,6 +70,50 @@ fn main() -> Result<()> {
                 volume::set(*vol)?;
             } else {
                 println!("{}", volume::get()?);
+            }
+        }
+        Some(("display", sub_matches)) => {
+            if let Some(sub_matches) = sub_matches.subcommand() {
+                match sub_matches {
+                    ("brightness", sub_matches) => {
+                        if let Some(brightness) = sub_matches.get_one::<i32>("BRIGHTNESS") {
+                            display::set_brightness(*brightness)?;
+                        } else {
+                            println!("{}", display::get_brightness()?);
+                        }
+                    }
+                    ("lumination", sub_matches) => {
+                        if let Some(lumination) = sub_matches.get_one::<i32>("LUMINATION") {
+                            display::set_lumination(*lumination)?;
+                        } else {
+                            println!("{}", display::get_lumination()?);
+                        }
+                    }
+                    ("hue", sub_matches) => {
+                        if let Some(hue) = sub_matches.get_one::<i32>("HUE") {
+                            display::set_hue(*hue)?;
+                        } else {
+                            println!("{}", display::get_hue()?);
+                        }
+                    }
+                    ("saturation", sub_matches) => {
+                        if let Some(saturation) = sub_matches.get_one::<i32>("SATURATION") {
+                            display::set_saturation(*saturation)?;
+                        } else {
+                            println!("{}", display::get_saturation()?);
+                        }
+                    }
+                    ("contrast", sub_matches) => {
+                        if let Some(contrast) = sub_matches.get_one::<i32>("CONTRAST") {
+                            display::set_contrast(*contrast)?;
+                        } else {
+                            println!("{}", display::get_contrast()?);
+                        }
+                    }
+                    _ => unreachable!(),
+                }
+            } else {
+                unreachable!()
             }
         }
         _ => unreachable!(),
