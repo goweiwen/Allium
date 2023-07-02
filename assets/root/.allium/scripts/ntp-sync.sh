4e8072bdce9f34a3f7fb68a818f5c9390f1de1ef
Goh Wei Wen <goweiwen@gmail.com>
2023-07-03 06:20:06 +0800

    feat: set hwclock on ntpd sync

--- assets/root/.allium/scripts/ntp-sync.sh
+++ assets/root/.allium/scripts/ntp-sync.sh
@@ -2,7 +2,7 @@
 
 if /mnt/SDCARD/.allium/scripts/wait-for-wifi.sh; then
     cd /mnt/SDCARD/
-    if ntpd -nq -p 0.pool.ntp.org -p 1.pool.ntp.org -p 2.pool.ntp.org; then
+    if ntpd -nq -S "/sbin/hwclock -w -u" -p 0.pool.ntp.org -p 1.pool.ntp.org -p 2.pool.ntp.org; then
         exit 0
     fi
 fi
