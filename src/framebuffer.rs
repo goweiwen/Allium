80bf7ed588450e3d4f41fe5f8e4a856162684e2d
Goh Wei Wen <goweiwen@gmail.com>
2024-06-05 00:42:27 +0800

    feat: rotate framebuffer

--- src/framebuffer.rs
+++ src/framebuffer.rs
@@ -1,19 +1,43 @@
+use std::{cell::OnceCell, env};
+
 use bytemuck::cast_slice;
 
 use crate::{HEIGHT, WIDTH};
 
 pub struct Framebuffer {
     fb: framebuffer::Framebuffer,
+    rotated_buf: Vec<u32>,
+    should_rotate: OnceCell<bool>,
 }
 
 impl Framebuffer {
     pub fn new() -> Self {
         let fb = framebuffer::Framebuffer::new("/dev/fb0").unwrap();
+        let should_rotate = OnceCell::new();
+        let rotated_buf = if *should_rotate.get_or_init(|| env::var("ROTATE").is_ok()) {
+            vec![0; WIDTH as usize * HEIGHT as usize]
+        } else {
+            vec![]
+        };
 
-        Self { fb }
+        Self {
+            fb,
+            rotated_buf,
+            should_rotate,
+        }
     }
 
     pub fn draw(&mut self, buffer: &[u32]) {
-        self.fb.frame[0..WIDTH as usize * HEIGHT as usize * 4].copy_from_slice(cast_slice(buffer));
+        let buf = if *self
+            .should_rotate
+            .get_or_init(|| env::var("ROTATE").is_ok())
+        {
+            self.rotated_buf.copy_from_slice(buffer);
+            self.rotated_buf.reverse();
+            &self.rotated_buf
+        } else {
+            buffer
+        };
+        self.fb.frame[0..WIDTH as usize * HEIGHT as usize * 4].copy_from_slice(cast_slice(buf));
     }
 }
