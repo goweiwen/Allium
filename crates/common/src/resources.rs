c9de1b1d16c8424c5e41eeffa80bc91ad04649b6
Wei Wen Goh <goweiwen@gmail.com>
2025-11-26 22:06:37 +0800

    refactor: switch Arc<RwLock> to Rc<RefCell>

--- crates/common/src/resources.rs
+++ crates/common/src/resources.rs
@@ -1,26 +1,30 @@
-use std::sync::Arc;
+use std::{
+    cell::{Ref, RefCell},
+    rc::Rc,
+};
 
 use log::trace;
-use parking_lot::{MappedRwLockReadGuard, RwLock, RwLockReadGuard};
 use type_map::TypeMap;
 
 #[derive(Debug, Clone)]
-pub struct Resources(pub Arc<RwLock<TypeMap>>);
+pub struct Resources(pub Rc<RefCell<TypeMap>>);
 
 impl Resources {
     /// Creates a new resource map.
     pub fn new(map: TypeMap) -> Self {
-        Self(Arc::new(RwLock::new(map)))
+        Self(Rc::new(RefCell::new(map)))
     }
 
     /// Gets a ref to a resource from the resource map. Panics if the resource is not present.
-    pub fn get<T: 'static>(&self) -> MappedRwLockReadGuard<'_, T> {
+    pub fn get<T: 'static>(&self) -> Ref<'_, T> {
         trace!("getting ref to resource: {:?}", std::any::type_name::<T>());
-        RwLockReadGuard::map(self.0.read(), |x| x.get::<T>().unwrap())
+        Ref::map(self.0.borrow(), |m| {
+            m.get::<T>().expect("resource not found")
+        })
     }
 
     /// Sets a resource in the resource map.
     pub fn insert<T: 'static>(&self, value: T) {
-        self.0.write().insert(value);
+        self.0.borrow_mut().insert::<T>(value);
     }
 }
